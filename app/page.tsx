1342626d0d614bedbd4add78ae69fc7e34f2b7f1
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2023-05-05 00:57:47 +0200

    Updated app.tsx token handling
    * added no-cache, because backend handles token caching (through redis and global state)

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import { SpotifyToken } from "./api/spotify/token/route";
 import { HomeComponent } from "@/components/pages/Home";
 
 async function getToken() {
-    const res = await fetch("http://localhost:3000/api/spotify/token");
+    // need the functionality of refreshing the token, when its expired!
+    // maybe use a store or something
+    const res = await fetch("http://localhost:3000/api/spotify/token", {
+        cache: "no-cache",
+    });
     return (await res.json()) as SpotifyToken;
 }
 
