687e9e505f9eb373e659fc3d27666c9ab191db42
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2023-05-05 00:57:00 +0200

    Updated tracks and recommend route to use pocketbase

--- app/api/spotify/recommend/route.ts
+++ app/api/spotify/recommend/route.ts
@@ -1,26 +1,30 @@
-import { connect } from '@/lib/mongo';
-import { ITrack, Track } from '@/models/Track';
+import { pb } from '@/lib/pocketbase';
+import { ISong, SongModel } from '@/models/Song';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
-    await connect();
+    const song: ISong = await request.json();
 
-    try {
-        const track: ITrack = await request.json();
+    if (!song) return NextResponse.json({ error: "Song is empty" }, { status: 400 });
 
-        const trackExists = await Track.exists({ id: track.id });
-
-        if (trackExists) {
-            await Track.updateOne({ id: track.id }, { $inc: { recommendations: 1 } });
-
-            return NextResponse.json({ message: `Song with id ${track.id} got incremented recommendation` });
+    // first check, if song already exists => update total_recommendations
+    const songRecord = await pb.collection("songs").getFirstListItem<SongModel>(`name=${song.name}`);
+    if (songRecord) {
+        try {
+            await pb.collection("songs").update(songRecord.id, {
+                total_recommendations: songRecord.total_recommendations + 1
+            });
+            return NextResponse.json({ message: "Song recommended" }, { status: 200 });
+        } catch (error) {
+            return NextResponse.json({ error }, { status: 500 });
         }
-
-        await Track.create(track);
-
-        return NextResponse.json({ message: `Song with id ${track.id} was recommended successfully` });
     }
-    catch (error) {
-        return NextResponse.json({ error: error }, { status: 500 });
+
+    // create new song
+    try {
+        await pb.collection("songs").create<ISong>(song);
+        return NextResponse.json({ message: "Song recommended" }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 });
     }
 }
--- app/api/spotify/tracks/route.ts
+++ app/api/spotify/tracks/route.ts
@@ -1,36 +1,28 @@
-import { connect } from "@/lib/mongo";
-import { Track } from "@/models/Track";
+import { pb } from "@/lib/pocketbase";
+import { ISong } from "@/models/Song";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+    // move to json body?
     const genres = request.nextUrl.searchParams.getAll('genres');
     const tags = request.nextUrl.searchParams.getAll('tags');
-    const start = request.nextUrl.searchParams.get('start');
+    const start = Number(request.nextUrl.searchParams.get('start')) || 1;
 
     // if genres or tags is empty, return error
     if (genres.length === 0 || tags.length === 0) {
         return NextResponse.json({ error: 'genres or tags is empty' }, { status: 400 })
     }
 
-    await connect();
+    // ref: https://github.com/pocketbase/pocketbase/discussions/1823#discussioncomment-4935299
+    const genreQuery = genres.map(genre => 'genres=' + genre).join(' || ');
+    const tagQuery = tags.map(tag => 'tags=' + tag).join(' || ');
 
-    // Note: this endpoint uses a limit of 20 items. This is because of the possible performance issue of going through all the tracks and their tags and genres.
-    // The tags and genres already have an index on them, but it's still a lot of data to go through. (might need to be tested)
-    try {
-        const startPosition = parseInt(start ?? "0");
-        const numItemsToLoad = 20;
+    const songsRecord = await pb.collection("songs").getList<ISong>(start, 50, {
+        filter: `${genreQuery} && ${tagQuery}`,
+    });
 
-        const tracks = await Track.find({
-            genres: { $in: genres },
-            tags: { $in: tags },
-        })
-            .skip(startPosition)
-            .limit(numItemsToLoad);
+    // could also return ListResult<ISong> to better evaluate the next pages and total count on client-side
+    const songs = songsRecord.items;
 
-
-        return NextResponse.json(tracks);
-    }
-    catch (error) {
-        return NextResponse.json({ error: error }, { status: 500 });
-    }
+    return NextResponse.json({ songs });
 }
