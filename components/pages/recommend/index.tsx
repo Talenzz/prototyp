3970658983a13b370475466e95a33b83007499da
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2023-05-14 18:36:45 +0200

    Moved SongSearch from Home to recommend component

--- components/pages/recommend/index.tsx
+++ components/pages/recommend/index.tsx
@@ -2,11 +2,11 @@ import { SpotifyToken } from "@/app/api/spotify/token/route";
 import { SongSearch } from "@/components/SongSearch";
 import { HeroTitle } from "@/components/layout/Hero";
 
-type HomeComponentProps = {
+type RecommendComponentProps = {
     token: SpotifyToken;
 };
 
-export function HomeComponent({ token }: HomeComponentProps) {
+export function RecommendComponent({ token }: RecommendComponentProps) {
     return (
         <>
             <HeroTitle />
