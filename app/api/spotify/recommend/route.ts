ed06641529c9ca1ce7a0e2b7d898b328a92f0388
Benjamin Schober <benjamin.schober@stud.hs-kempten.de>
2023-05-14 18:43:07 +0200

    Fixed filters in recommend route
    * to check for an existing item, ``getFirstListItem`` cant be used (throws 404), instead ``getList`` should be used. if the item exists, ``getList`` can be user to update the item
    * Note: Various changes will be done in the future: Songs can be submitted multiple times in different genres and tags. They should not be merged. only metadata and ``total_recommendations`` and ``total_likes`` are stored central, the "real" recommendation links back to song meta

--- app/api/spotify/recommend/route.ts
+++ app/api/spotify/recommend/route.ts
@@ -8,10 +8,14 @@ export async function POST(request: NextRequest) {
     if (!song) return NextResponse.json({ error: "Song is empty" }, { status: 400 });
 
     // first check, if song already exists => update total_recommendations
-    const songRecord = await pb.collection("songs").getFirstListItem<SongModel>(`name=${song.name}`);
-    if (songRecord) {
+    const records = await pb.collection("songs").getList<SongModel>(1, 50, {
+        filter: `name="${song.name}"`
+    });
+    if (records.items.length > 0) {
         try {
+            const songRecord = await pb.collection("songs").getFirstListItem<SongModel>(`name="${song.name}"`);
             await pb.collection("songs").update(songRecord.id, {
+                tags: songRecord.tags.concat(song.tags),
                 total_recommendations: songRecord.total_recommendations + 1
             });
             return NextResponse.json({ message: "Song recommended" }, { status: 200 });
